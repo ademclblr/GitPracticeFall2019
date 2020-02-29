520408d8057d182aebe55f9cdc51b9185d918c40
adem <ademclblr@gmail.com>
2020-02-29 13:40:19 -0700

    my first commit

---
+++ src/day1/loopPractice.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class loopPractice {
+    public static void main(String[] args) {
+        int age = 5;
+        for (int i = 0; i <5 ; i++) {
+            System.out.println(i);
+
+
+        }
+        System.out.println(age);
+    }
+}
