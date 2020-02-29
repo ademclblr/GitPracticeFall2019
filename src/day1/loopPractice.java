b7d16865dd4041200f12258bdb314ff5588fe9f1
adem <ademclblr@gmail.com>
2020-02-29 13:51:05 -0700

    aptade

--- src/day1/loopPractice.java
+++ src/day1/loopPractice.java
@@ -3,7 +3,7 @@ package day1;
 public class loopPractice {
     public static void main(String[] args) {
         int age = 5;
-        for (int i = 0; i <5 ; i++) {
+        for (int i = 0; i <=5 ; i++) {
             System.out.println(i);
 
 
