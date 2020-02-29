9bdf26fe9539662b0f4226ff5ac6266ffaa33256
adem <ademclblr@gmail.com>
2020-02-29 13:25:38 -0700

    modified StringPractice

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -3,7 +3,8 @@ package day1;
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("hello world!");
-
+        String str="java is fine!";
+        System.out.println(str);
 
     }
 }
